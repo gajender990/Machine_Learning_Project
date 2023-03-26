f70e299288d23944d62d5c0572ddbf58d5cf8bc2
gajender990 <kunwar.gajender34@gmail.com>
2023-03-26 12:59:21 +0530

    Data Ingestion and Data Transformation Done

--- src/logger.py
+++ src/logger.py
@@ -4,19 +4,21 @@ from datetime import datetime
 
 ## Creating log file
 ## Naming convention of log file
-LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+LOG_FILE=f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
 
 # place and name where log file will get created
-logs_path = os.path.join(os.getcwd(),"","logs",LOG_FILE)
+logs_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
 
 # Creation of log file(if file exist append it)
 os.makedirs(logs_path,exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(logs_path, LOG_FILE)
+LOG_FILE_PATH=os.path.join(logs_path,LOG_FILE)
 
 ## Override logging so that it can be imported
 logging.basicConfig(
-    filename== LOG_FILE_PATH,
-    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO()
+    filename=LOG_FILE_PATH,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
 )
+
+
